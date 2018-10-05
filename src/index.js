274a640fc6ae4ae62f1d6fd6a879e68d5a91764d
evpodrezenko <evpodrezenko@gmail.com>
2018-10-06 02:51:04 +0300

    find length of the longest consecutive

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let sortedArray = array.sort((a, b) => a - b);
+  return find(sortedArray);
+}
+
+function find(array) {
+  let maxLength = 0;
+  let length = 1;
+  const arrLength = array.length;
+ 
+  if (arrLength < 2) {
+    return arrLength;
+  }
+
+  for (let i = 1; i < arrLength; i++) {
+    const step = array[i] - array[i - 1];
+    if (step === 1) {
+      length++;
+    } else if (step > 1) {
+      if (length > maxLength) {
+        maxLength = length;
+      }
+      length = 1;
+    }
+  }
+  return maxLength;
 }
